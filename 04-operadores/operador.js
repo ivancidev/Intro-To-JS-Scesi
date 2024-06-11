08452002d94d7021d66e9630a035bcb4a37fc5cf
Herlan <herlanherbas@gmail.com>
2024-06-11 00:56:45 -0400

    create operator

---
+++ 04-operadores/operador.js
@@ -0,0 +1,23 @@
+//operadores numericos
+console.log(6 >= 6);
+console.log(6 === "6"); // ===
+
+//operadores logicos
+console.log(true && true);
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+
+console.log(!false);
+console.log((true && false) || false);
+
+//operadores aritmeticos
+console.log(5 + 3); 
+console.log(5 - 3); 
+console.log(5 * 3); 
+console.log(5 / 3); 
+console.log(2 ** 2); // otra forma de potencia
+
+//operadores de texto
+console.log("a" == "a"); 
+console.log("a" != "b"); 
