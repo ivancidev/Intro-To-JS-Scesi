918ec1a07cfd152b168c52219ac389b31fd1985a
Herlan <herlanherbas@gmail.com>
2024-06-11 00:56:22 -0400

    create types

---
+++ 01-tipos-de-variable/tipo.js
@@ -0,0 +1,65 @@
+// let variable local con ámbito de bloque
+// const Variable constante
+// var variable local y global, dependiendo del contexto de ejecución.
+
+const PI = 3.14; // Variable constante
+var edad = 30; // Variable publica
+// let nombre = "Juan"; // Variable que puede cambiar
+
+let nombre = null;
+let celular;
+console.log("nombre: ", nombre);
+console.log("PI: ", PI);
+console.log("edad: ", edad);
+console.log("edad: " + edad);
+
+console.log(nombre * 2);
+console.log(celular);
+console.log(typeof nombre);
+console.log(typeof celular);
+console.log(typeof PI);
+console.log(typeof edad);
+
+console.log(animal);
+var animal = "gato";
+
+//formas de nombrar una variable
+// camel case (mas recomendado)
+let esPar;
+// pascal case
+let Persona;
+// snake case
+let mi_nombre;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
